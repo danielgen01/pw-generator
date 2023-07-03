17ad1270c05a7c0615c72a6544da2ac3ba6aa417
Daniel <gendik38@gmail.com>
2023-07-03 14:19:19 +0200

    implement the "copy-pw" button

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,19 +1,24 @@
 import React from "react"
 import { IoReload } from "react-icons/io5"
+import { AiOutlineCopy } from "react-icons/ai"
 
 const Hero: React.FC = () => {
 	return (
-		<section className="px-10 py-10 flex flex-col">
-			<div className="generated-passwort h-20 bg-[#00F0FF19] border-bright-aque border-2 outline-none px-10 flex items-center justify-between">
+		<section className="px-10 py-10 flex flex-col items-start gap-10">
+			<div className="generated-passwort h-20 bg-[#00F0FF19] border-bright-aque border-2 outline-none px-10 flex items-center justify-between w-full">
 				<input
 					type="text"
 					className=" tracking-[3.2px] font-normal bg-transparent outline-none text-bright-aque w-3/4"
-					readOnly={false}
+					readOnly={true}
+					placeholder="la5642xt0#ßßgg!"
 				/>
 				<button className="hover:opacity-70 duration-150 w-1/4 flex justify-end">
 					<IoReload className="text-bright-aque text-3xl font-bold" />
 				</button>
 			</div>
+			<button className="uppercase bg-neon-yellow w-full h-24 text-black text-xl font-semibold tracking-[3.2px] flex justify-between items-center px-10 rounded-bl-3xl hover:opacity-75 duration-150">
+				copy password_ <AiOutlineCopy className="text-2xl" />
+			</button>
 		</section>
 	)
 }

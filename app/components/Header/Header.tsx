3fcf78ebc736395c033b457b3b4a1dccb4c72a7c
Daniel <gendik38@gmail.com>
2023-07-03 13:56:54 +0200

    add + style header section

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react"
 
-const Header:React.FC = () => {
-  return (
-    <header className='flex w-screen justify-center py-20'>
-        <h1 className='uppercase text-neon-yellow text-4xl font-bold'>password generator</h1>
-        
-        </header>
-  )
+const Header: React.FC = () => {
+	return (
+		<header className="flex w-screen justify-center py-10">
+			<h1 className="uppercase text-neon-yellow text-4xl font-bold tracking-[8px]">
+				password <br /> generator
+			</h1>
+		</header>
+	)
 }
 
 export default Header

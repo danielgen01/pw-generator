4482cd32580a3ccbd2f617d3673258610d09eb00
Daniel <gendik38@gmail.com>
2023-07-03 14:23:44 +0200

    add some b-radius to the pw div

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import { AiOutlineCopy } from "react-icons/ai"
 const Hero: React.FC = () => {
 	return (
 		<section className="px-10 py-10 flex flex-col items-start gap-10">
-			<div className="generated-passwort h-20 bg-[#00F0FF19] border-bright-aque border-2 outline-none px-10 flex items-center justify-between w-full">
+			<div className="generated-passwort h-20 bg-[#00F0FF19] border-bright-aque border-2 outline-none px-10 flex items-center justify-between w-full  rounded-md">
 				<input
 					type="text"
 					className=" tracking-[3.2px] font-normal bg-transparent outline-none text-bright-aque w-3/4"
@@ -16,6 +16,7 @@ const Hero: React.FC = () => {
 					<IoReload className="text-bright-aque text-3xl font-bold" />
 				</button>
 			</div>
+
 			<button className="uppercase bg-neon-yellow w-full h-24 text-black text-xl font-semibold tracking-[3.2px] flex justify-between items-center px-10 rounded-bl-3xl hover:opacity-75 duration-150">
 				copy password_ <AiOutlineCopy className="text-2xl" />
 			</button>

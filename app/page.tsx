69ac84eeff413ee40281de7860e2bbf5f7d67265
Daniel <gendik38@gmail.com>
2023-07-03 14:53:01 +0200

    add the hero comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
 
 export default function App() {
   return (
    <>
    <Header />
    <main>
-
+    <Hero />
 
 
    </main>
